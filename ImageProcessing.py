9a0e2f55a7273f45066c0316f17d2a7290e870f3
MingyunKang <kangmingyun@gangmingyuns-MacBook-Pro.local>
2023-03-07 17:09:33 +0900

    variables changed

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -10,9 +10,9 @@ height = 3024
 pts1 = np.float32([[460,310], [570,2340], [2590, 640], [2530,1820]])
 pts2 = np.float32([[0,0], [0, height], [width, 0],[width, height]])
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
-img = cv2.warpPerspective(src, matrix, (width, height))
+homography = cv2.warpPerspective(src, matrix, (width, height))
 #
-rows = 1
+rows = 2
 cols = 2
 fig = plt.figure(figsize=(12,6))
 ax1 = fig.add_subplot(rows, cols, 1)
@@ -22,16 +22,15 @@ for k in range(4):
 ax1.set_title('Original Image')
 ax1.axis("off")
 ax2 = fig.add_subplot(rows, cols, 2)
-ax2.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+ax2.imshow(cv2.cvtColor(homography, cv2.COLOR_BGR2RGB))
 ax2.set_title('Homography')
 ax2.axis("off")
-plt.show()
 
 ############################ Hough line Transform ########################################
 
 
-gray = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,150,220,apertureSize = 3)
+gray = cv2.cvtColor(homography,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,100,apertureSize = 3)
 
 lines = cv2.HoughLines(edges,1,np.pi/180,150)
 for line in lines:
@@ -45,16 +44,17 @@ for line in lines:
     x2 = int(x0 - 1000*(-b))
     y2 = int(y0 - 1000*(a))
 
-    cv2.line(src,(x1,y1),(x2,y2),(0,0,255),1)
-ax3 = fig.add_subplot(rows,cols,1)
+    houghline = cv2.line(homography,(x1,y1),(x2,y2),(0,0,255),1)
+ax3 = fig.add_subplot(rows,cols,3)
 ax3.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
-ax3.set_title('Canny Edge')
+ax3.set_title('Canny Edges')
 ax3.axis("off")
-ax4 = fig.add_subplot(rows,cols,2)
-ax4.imshow(cv2.cvtColor(src, cv2.COLOR_BGR2RGB))
-ax4.set_title('Hough Line')
+ax4 = fig.add_subplot(rows,cols,4)
+ax4.imshow(cv2.cvtColor(houghline, cv2.COLOR_BGR2RGB))
+ax4.set_title('Hough Line Transform')
 ax4.axis("off")
 plt.show()
+print(lines)
 
 
 
